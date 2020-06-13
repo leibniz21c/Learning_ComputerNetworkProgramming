eeab21061f737d644fdc7c5727cea77162e9762c
Heesung Yang <38760913+ndo04343@users.noreply.github.com>
2020-06-13 14:56:09 +0900

    2020.06.13.14:56

--- src/ch5/op_client.c
+++ src/ch5/op_client.c
@@ -7,55 +7,67 @@
 
 #define BUF_SIZE 1024
 #define RLT_SIZE 4
-#define OPSZ 4 // size of integer.
+#define OPSZ 4
+
 void error_handling(char* message);
 
 int main(int argc, char* argv[])
 {
 	int sock;
-	char opmsg[BUF_SIZE] = { 0, };
+	char opmsg[BUF_SIZE];
 	int result, opnd_cnt, i;
 	struct sockaddr_in serv_adr;
 
-	/* You have to press like this */
-	/* ./op_client <server_IP> <port> */
+
+	/* You have to press like this. */
+	/* ./op_client <IP> <port> */
 	if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
+		printf("Usage : %s <IP> <port>\n",argv[0]);
 		exit(1);
 	}
 
-	/* Creating Socket */
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1)
-		error_handling("socket() error");
-
-	/* Socket address struct memory setting */
+	/* Creating socket and initialize sock address structure */
+	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+		error_handling("socket() error.");
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]); // IP setting
-	serv.adr.sin_port = htons(atoi(argv[2]));
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
 
-	if (connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+	/* connecting with server */
+	if (connect(sock, (struct sockaddr*)serv_adr, sizeof(serv_adr)) == -1)
 		error_handling("connect() error!");
 	else
-		puts("Connected........");
+		puts("Connected......");
 
-
-	/* getting operands */
-	fputs("Operand count : ", stdout);
+	/*                               */
+	/* Get the information from user */
+	/*                               */
+	fputs("Operand count: ", stdout);
 	scanf("%d", &opnd_cnt);
-	opmsg[0] = (char)opnd_cnt; // This data structure will be used. Like paskal.
-
-	for (i = 0; i < opnd_cnt; i++) {
-		printf("Operand %d: ", i+1);
-		scanf("%d", (int*)&opmsg[i * OPSZ + 1]); // We have to send 1 byte/ 4 bytes ..../ 4 bytes.
+	opmsg[0] = (char)opnd_cnt; /* Like paskal string method */
+	for (i = 0; i < opnd_cnt, i++) {
+		printf("Operand %d: ", i+1 );
+		scanf("%d", (int*)&opmsg[i*OPSZ+1]);
 	}
-	fgetc(stdin); // get '\n'
-
-	/* getting operaotor */
+	fgetc(stdin);
 	fputs("Operator: ", stdout);
-	scanf("%c", &opmsg[opnd_cnt * OPSZ + 1]);
-	
+	scanf("%c", &opmsg[opnd_cnt*OPSZ+1]);
+	/*                               */
+	/*                               */
 
 
+	/* Transmitting and getting data */
+	write(sock, opmsg, opnd_cnt*OPSZ+2);
+	read(sock, &result, RLT_SIZE);
+
+	printf("Operation result: %d \n", result);
+	close(sock);
+	return 0;
+}
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
